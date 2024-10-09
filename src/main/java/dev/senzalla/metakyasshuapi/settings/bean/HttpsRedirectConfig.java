0a370a49d24cbf75666cdc70bca1773b824c9102
bomsalvez <bomsalvez@gmail.com>
2024-10-09 08:17:27 -0400

    criacao de ssl

--- src/main/java/dev/senzalla/metakyasshuapi/settings/bean/HttpsRedirectConfig.java
+++ src/main/java/dev/senzalla/metakyasshuapi/settings/bean/HttpsRedirectConfig.java
@@ -1,26 +1,26 @@
-package dev.senzalla.metakyasshuapi.settings.bean;
-
-import org.apache.catalina.Context;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class HttpsRedirectConfig {
-    @Bean
-    public TomcatServletWebServerFactory servletContainer() {
-        return new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-    }
-}
+//package dev.senzalla.metakyasshuapi.settings.bean;
+//
+//import org.apache.catalina.Context;
+//import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+//import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+//import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class HttpsRedirectConfig {
+//    @Bean
+//    public TomcatServletWebServerFactory servletContainer() {
+//        return new TomcatServletWebServerFactory() {
+//            @Override
+//            protected void postProcessContext(Context context) {
+//                SecurityConstraint securityConstraint = new SecurityConstraint();
+//                securityConstraint.setUserConstraint("CONFIDENTIAL");
+//                SecurityCollection collection = new SecurityCollection();
+//                collection.addPattern("/*");
+//                securityConstraint.addCollection(collection);
+//                context.addConstraint(securityConstraint);
+//            }
+//        };
+//    }
+//}
