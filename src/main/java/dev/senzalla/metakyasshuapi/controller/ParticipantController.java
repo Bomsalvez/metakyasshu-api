9571baa03cabf7ac8390bdbc4d1937f6c905b46b
bomsalvez <bomsalvez@gmail.com>
2024-10-15 20:12:24 -0400

    feat: declaracao de participante

--- src/main/java/dev/senzalla/metakyasshuapi/controller/ParticipantController.java
+++ src/main/java/dev/senzalla/metakyasshuapi/controller/ParticipantController.java
@@ -3,7 +3,6 @@ package dev.senzalla.metakyasshuapi.controller;
 import dev.senzalla.metakyasshuapi.model.participation.module.ParticipationDto;
 import dev.senzalla.metakyasshuapi.model.participation.module.ParticipationForm;
 import dev.senzalla.metakyasshuapi.service.expense.ExpenseService;
-import dev.senzalla.metakyasshuapi.service.participant.ParticipantService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,10 +14,9 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/participant")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class ParticipantController {
-    private final ParticipantService service;
     private final ExpenseService expenseService;
 
-    @PostMapping
+    @PostMapping("/expense")
     public ResponseEntity<ParticipationDto> registerParticipant(@RequestBody @Validated ParticipationForm form, @RequestParam boolean recalculateParticipation) {
         ParticipationDto dto = expenseService.addParticipant(form, recalculateParticipation);
         return ResponseEntity.status(HttpStatus.CREATED).body(dto);
